27df2e3aad3930bacab972e35262da78c7d6a0b2
nhanhas <mpereira132@hotmail.com>
2018-02-10 15:30:42 +0000

    S#03 - Sincro done

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 
 require('libs/excel-reader/php-excel-reader/excel_reader2.php');
 require('libs/excel-reader/SpreadsheetReader.php');
@@ -6,7 +7,25 @@ require('libs/excel-reader/SpreadsheetReader.php');
 //Define constants
 define("excelFile", 'LegoWear-PS18.xlsx'); //Index of Excel Sheet to Read
 define("sheetToRead", 4); //Index of Excel Sheet to Read
+define("backendUrl", "https://sis04.drivefx.net/E252DC0A/PHCWS/REST");//TODO change for client
+$_SESSION['driveCredentials'] = array(
+	userCode=>"suporte",
+	password=>"12345678",
+	applicationType=>"HYU45F-FKEIDD-K93DUJ-ALRNJE",
+	company=>""
+);
 
+
+$ch = curl_init();
+
+//First Login at Drive
+$loginResult = DRIVE_userLogin();
+if($loginResult == false){
+	$msg = "Error on Drive Login.<br>";
+	echo $msg;
+	logData($msg);
+	exit(1);
+}
 
 $msg = "#1 - Read Excel<br>";
 echo $msg;
@@ -15,14 +34,215 @@ logData($msg);
 //#1 - Read Excel
 $products = EXCEL_reader();
 
-print_r(json_encode($products));
+$msg = "#2 - Start import<br>";
+echo $msg;
+logData($msg);
+
+//#2 - Start import
+foreach($products as $product){
+
+    //#1 - check if ref is greater than 18 chars
+    if(sizeof($product->ref) > 18){
+        $msg = "Product with ref - ". $product->ref ." is too long (>18 chars)<br><br>";
+        echo $msg;
+        logData($msg);
+        continue;
+    }
+
+    $msg = "Sync: <br>". json_encode($product) ."<br>";
+    echo $msg;
+    logData($msg);
+
+    //#2 - Get a new instance of an St
+    $newInstanceSt = DRIVE_getNewInstance("St", 0);
+    if($newInstanceSt == null){
+        $msg = "Error on getting new instance ST. <br><br>";
+        echo $msg;
+        logData($msg);
+        continue;
+    }
+
+    //#3 - fulfill properties
+    $newInstanceSt['ref']           = $product->ref;
+    $newInstanceSt['codigo']        = $product->codigo;
+    $newInstanceSt['design']        = $product->design;
+    $newInstanceSt['desctec']       = $product->desctec;
+    $newInstanceSt['epv1']          = $product->epv1;
+    $newInstanceSt['iva1incl']      = $product->iva1incl;
+    $newInstanceSt['familia']       = $product->familia;
+    $newInstanceSt['usr2']          = $product->modelo;
+    $newInstanceSt['usr1']          = $product->marca;
+
+    //#3.1 - fulfill extensions fields
+    $newInstanceSt['u6526_st_extra_fields']['ref_base']     = $product->refBase;
+    $newInstanceSt['u6526_st_extra_fields']['design_en']    = $product->designEN;    
+    $newInstanceSt['u6526_st_extra_fields']['desctec_en']   = $product->desctecEN;
+
+
+    //#4 - an sync entity
+    $newInstanceSt = DRIVE_actEntiy("St", $newInstanceSt);
+    if($newInstanceSt == null){
+        $msg = "Error on act entity for product name = " .$product->design . " <br><br>";
+        echo $msg;
+        logData($msg);
+        continue;
+    }
+
+    //#5 - Save product
+    $newInstanceSt = DRIVE_saveInstance("St", $newInstanceSt);
+    if($newInstanceSt == null){
+        $msg = "Error on save for product name = " .$product->design . " <br><br>";
+        echo $msg;
+        logData($msg);
+        continue;
+    }
+
+    $msg = "Product created with ref = " .$newInstanceSt['ref']. " <br><br>";
+    echo $msg;
+    logData($msg);
+
+    exit(1);    
+}
 
 
 
 
 
+/**
+ * DRIVE WS
+ */
+//Get New Instance (Entity= Cl , Bo, St)
+function DRIVE_getNewInstance($entity, $ndos){
+
+	global $ch;
+
+	$url = backendUrl . "/".$entity."WS/getNewInstance";
+	$params =  array('ndos' => $ndos);
+
+	$response=DRIVE_Request($ch, $url, $params);
+
+	if(empty($response)){
+		return null;
+	}
+	if(isset($response['messages'][0]['messageCodeLocale'])){
+		return null;
+	}
 
 
+	return $response['result'][0];
+}
+
+//Sync entity Instance (Entity= Cl , Bo, St)
+function DRIVE_actEntiy($entity, $itemVO){
+
+	global $ch;
+
+	$url = backendUrl . "/".$entity."WS/actEntity";
+	$params =  array('entity' => json_encode($itemVO),
+					 'code' => 0,
+					 'newValue' => json_encode([])
+				);
+
+	$response=DRIVE_Request($ch, $url, $params);
+
+	//echo json_encode( $response );
+	if(empty($response)){
+		return null;
+	}
+	if(isset($response['messages'][0]['messageCodeLocale']) && $response['messages'][0]['messageCode'] != 'messages.Business.Stocks.InvalidRefAutoCreate'){
+		$msg = $response['messages'][0]['messageCodeLocale'];
+		logData($msg);
+		return null;
+	}
+
+
+	return $response['result'][0];
+}
+
+//save Instance (Entity= Cl , Bo, St)
+function DRIVE_saveInstance($entity, $itemVO){
+
+	global $ch;
+
+	$url = backendUrl .  "/".$entity."WS/Save";
+	$params =  array('itemVO' => json_encode($itemVO),
+					 'runWarningRules' => 'false'
+				);
+
+	$response=DRIVE_Request($ch, $url, $params);
+
+	//echo json_encode( $response );
+	if(empty($response)){
+		$msg = "Empty save";
+		logData($msg);
+		return null;
+	}
+	if(isset($response['messages'][0]['messageCodeLocale'])){
+		$msg = $response['messages'][0]['messageCodeLocale'];
+		logData($msg);
+		return null;
+	}
+
+
+	return $response['result'][0];
+}
+
+// Drive Generic call
+function DRIVE_Request($ch, $url,$params){
+
+	// Build Http query using params
+	$query = http_build_query ($params);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, false);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+	curl_setopt($ch, CURLOPT_BINARYTRANSFER, false);
+
+
+	$response = curl_exec($ch);
+	// send response as JSON
+	return json_decode($response, true);
+}
+
+//Call Login
+function DRIVE_userLogin(){
+	global $ch;
+
+	$url = backendUrl . '/UserLoginWS/userLoginCompany';
+
+	// Create map with request parameters
+	$params = $_SESSION['driveCredentials'];
+
+	// Build Http query using params
+	$query = http_build_query ($params);
+	//initial request with login data
+
+	//URL to save cookie "ASP.NET_SessionId"
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	//Parameters passed to POST
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, '');
+	curl_setopt($ch, CURLOPT_COOKIEFILE, '');
+	$response = curl_exec($ch);
+
+	// send response as JSON
+	$response = json_decode($response, true);
+	if (curl_error($ch)) {
+		return false;
+	} else if(empty($response)){
+		return false;
+	} else if(isset($response['messages'][0]['messageCodeLocale'])){
+		echo $response['messages'][0]['messageCodeLocale']."<br>";
+		echo "Error in login. Please verify your username, password, applicationType and company." ;
+		return false;
+	}
+	return true;
+}
 
 
 /**
@@ -56,7 +276,6 @@ function EXCEL_reader(){
         
 }
 
-
 /**
  * Class Product
  */
@@ -68,7 +287,7 @@ class Product {
     public $design = '';    
     public $desctec = '';    
     public $epv1 = 0;
-    public $ivaincl1 = true;
+    public $iva1incl = true;
     public $familia = '';
     public $modelo = '';
     public $marca = '';    
